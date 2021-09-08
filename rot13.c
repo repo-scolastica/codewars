80369101621b2b7608f8cd53655a9d1b09f4f4d8
repo-scolastica <90068724+repo-scolastica@users.noreply.github.com>
2021-09-09 00:31:51 +0200

    Add files via upload

---
+++ rot13.c
@@ -0,0 +1,18 @@
+/* https://www.codewars.com/kata/530e15517bc88ac656000716 */
+#include <string.h>
+#include <stddef.h>
+#include <ctype.h>
+
+char *rot13(const char *src)
+{
+    char *encrypt = (char *)calloc(sizeof(src), sizeof(char));
+    if (!encrypt) { perro("ENOMEM"); return NULL; }
+    strncpy(encrypt, src, sizeof(src));
+
+    while (*current) { 
+        if (isalpha(*current)) { *current += (*current | 32) < 'n' ? 13 : - 13; }
+        ++current;
+    } 
+
+    return encrypt;
+}
